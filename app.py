357f50adbbcbbd149d0650d3653440d7622de44b
minimalsend <scannybeta@gmail.com>
2025-09-20 21:40:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,6 @@ class InstagramChatMonitor:
             desc = data.get("desc", "")
 
             if msg == "error_invalid_serialno":
-                self.sentel(code, chat_name)
                 return f"❌ Código inválido: {code}"
             elif msg == "error_already_redeemed":
                 self.sentel(code, chat_name)
@@ -274,5 +273,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
